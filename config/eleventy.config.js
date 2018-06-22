6a3c6edfa0fd43b272be9b5384d15520205790a2
Alex Carpenter <alex@mightyinthemidwest.com>
2018-06-22 07:23:00 -0400

    Its a start

--- config/eleventy.config.js
+++ config/eleventy.config.js
@@ -1,8 +1,25 @@
+const CleanCSS = require("clean-css");
+const UglifyJS = require("uglify-js");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addFilter("jsmin", function(code) {
+    let minified = UglifyJS.minify(code);
+    if( minified.error ) {
+      console.log("UglifyJS error: ", minified.error);
+      return code;
+    }
+    return minified.code;
+  });
 
   return {
     templateFormats: [
-      "njk"
+      "njk",
+      "md",
+      "html"
     ],
     dir: {
       input: "src",
---
+++ src/discord-thanks.md
@@ -0,0 +1,8 @@
+---
+layout: page.njk
+title: Success
+permalink: /discord/success/index.html
+---
+Thanks for signing up to join our Discord community. We will send you an invite link shortly. While your waiting please be sure to read our [Code of Conduct](/code-of-conduct). Thanks!
+
+– Alex
