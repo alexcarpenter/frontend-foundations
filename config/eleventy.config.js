d14e50943c2abbae8d7ba8f07853ac1b9f721193
Alex Carpenter <alex@mightyinthemidwest.com>
2018-07-26 21:32:01 -0400

    Adds previous newsletter issues

--- config/eleventy.config.js
+++ config/eleventy.config.js
@@ -1,61 +1,79 @@
-const { DateTime } = require('luxon');
-const slugify = require('@sindresorhus/slugify');
-const CleanCSS = require('clean-css');
-const UglifyJS = require('uglify-js');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
+const { DateTime } = require('luxon')
+const slugify = require('@sindresorhus/slugify')
+const CleanCSS = require('clean-css')
+const UglifyJS = require('uglify-js')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
 const markdown = require('markdown-it')({
   html: true,
   breaks: true,
   linkify: true,
   typographer: true
-});
+})
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
+  const parseDate = str => {
+    if (str instanceof Date) {
+      return str
+    }
+    const date = DateTime.fromISO(str, { zone: 'utc' })
+    return date.toJSDate()
+  }
 
-  eleventyConfig.addFilter('cssmin', code => new CleanCSS({}).minify(code).styles);
+  eleventyConfig.addPlugin(pluginRss)
+
+  eleventyConfig.addFilter(
+    'cssmin',
+    code => new CleanCSS({}).minify(code).styles
+  )
 
   eleventyConfig.addFilter('jsmin', code => {
-    let minified = UglifyJS.minify(code);
+    let minified = UglifyJS.minify(code)
     if (minified.error) {
-      console.log('UglifyJS error: ', minified.error);
-      return code;
+      console.log('UglifyJS error: ', minified.error)
+      return code
     }
-    return minified.code;
-  });
+    return minified.code
+  })
 
-  eleventyConfig.addFilter('markdownify', str => markdown.render(str));
-  eleventyConfig.addFilter('markdownify_inline', str => markdown.renderInline(str));
+  eleventyConfig.addFilter('markdownify', str => markdown.render(str))
+  eleventyConfig.addFilter('markdownify_inline', str =>
+    markdown.renderInline(str)
+  )
 
-  eleventyConfig.addFilter('slugify', str => slugify(str));
+  eleventyConfig.addFilter('slugify', str => slugify(str))
 
-  eleventyConfig.addFilter('permalink', str => str.replace(/\.html/g, ''));
+  eleventyConfig.addFilter('permalink', str => str.replace(/\.html/g, ''))
 
   eleventyConfig.addFilter('readableDate', dateObj =>
     DateTime.fromJSDate(dateObj).toFormat('dd LLL yyyy')
-  );
+  )
 
-  eleventyConfig.addNunjucksFilter('prepend', function(value, prepender) {
-    return value === '/' ? prepender : prepender + value;
-  });
+  eleventyConfig.addFilter('date_formatted', obj => {
+    const date = parseDate(obj)
+    return DateTime.fromJSDate(date).toFormat('yyyy/MM/dd')
+  })
+
+  eleventyConfig.addNunjucksFilter('prepend', (value, prepender) => {
+    return value === '/' ? prepender : prepender + value
+  })
 
   eleventyConfig.addCollection('jobs', collection => {
     return collection
       .getFilteredByGlob('**/jobs/*.md')
       .reverse()
-      .filter(item => item.data.status === 'open');
-  });
+      .filter(item => item.data.status === 'open')
+  })
 
   eleventyConfig.addCollection('posts', collection => {
-    return collection.getFilteredByGlob('**/posts/!(index)*.md').reverse();
-  });
+    return collection.getFilteredByGlob('**/posts/!(index)*.md').reverse()
+  })
 
-  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
-  eleventyConfig.addLayoutAlias('markdown', 'layouts/markdown.njk');
-  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
+  eleventyConfig.addLayoutAlias('markdown', 'layouts/markdown.njk')
+  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk')
 
-  eleventyConfig.addPassthroughCopy('src/icons');
-  eleventyConfig.addPassthroughCopy('src/sw.js');
+  eleventyConfig.addPassthroughCopy('src/icons')
+  eleventyConfig.addPassthroughCopy('src/sw.js')
 
   return {
     templateFormats: ['njk', 'md', 'html'],
@@ -69,5 +87,5 @@ module.exports = function(eleventyConfig) {
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: false,
     passthroughFileCopy: true
-  };
-};
+  }
+}
